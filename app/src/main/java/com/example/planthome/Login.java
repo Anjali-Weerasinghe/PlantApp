bd0fb37b23955703992af0828a9d1b7d6145ed4e
menaka809 <malithjayasekara2255721@gmail.com>
2021-05-07 11:30:38 +0530

    updated plant login

--- app/src/main/java/com/example/planthome/Login.java
+++ app/src/main/java/com/example/planthome/Login.java
@@ -65,7 +65,7 @@ public class Login extends AppCompatActivity {
         startActivity(intent3);
     }
     public void openUserInterface(){
-        Intent intent5=new Intent(this,UserInterface.class);
+        Intent intent5=new Intent(this,PlantManagerUserInterface.class);
         startActivity(intent5);
     }
     public void forgotPassword(){
