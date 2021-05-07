300a5abfdcc4e879a9a2f822ea4ff581e26ff99e
menaka809 <malithjayasekara2255721@gmail.com>
2021-05-07 11:28:29 +0530

    updated home java file

--- app/src/main/java/com/example/planthome/Home.java
+++ app/src/main/java/com/example/planthome/Home.java
@@ -6,16 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
+    ImageView cus_plant_view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        cus_plant_view=findViewById(R.id.plant_image_View);
 
 
         BottomNavigationView navigationView1=(BottomNavigationView) findViewById(R.id.navigation1);
@@ -36,5 +42,18 @@ public class Home extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cus_plant_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Home.this,Customer_view_plant_item.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
