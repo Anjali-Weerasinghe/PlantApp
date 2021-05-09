a1e3ff14ee6b5dfcab4454c6b6cbdec8e4bac6b2
IT19965864 <sajanaran@gmail.com>
2021-05-09 22:42:18 +0530

    creat new activity

---
+++ app/src/main/java/com/example/planthome/Delete_Tutotorials.java
@@ -0,0 +1,14 @@
+package com.example.planthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Delete_Tutotorials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete__tutotorials);
+    }
+}
