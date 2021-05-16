76d02dfe81b89b2d4e8cd4a24dabecc5c32c07a0
ANJALI WEERASINGHE <sapnaweerasinghe98@gmail.com>
2021-05-16 12:52:26 +0530

    Android Tests for Recycle View and intent

--- app/src/androidTest/java/com/example/planthome/AdminProfileNavigationTest.java
+++ app/src/androidTest/java/com/example/planthome/AdminProfileNavigationTest.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.intent.IntentStubber;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,6 +18,8 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -26,31 +30,20 @@ public class AdminProfileNavigationTest{
 
     private static final int ITEMBELOWFINDINLIST = 5;
     @Rule
-    public ActivityTestRule adminProfileNavigationActivityTestRule;
+    public ActivityTestRule<AdminProfileNavigation> adminProfileNavigationActivityTestRule = new ActivityTestRule<>(AdminProfileNavigation.class);
 
-    public AdminProfileNavigationTest() {
-        adminProfileNavigationActivityTestRule = new ActivityTestRule<>(AdminProfileNavigation.class);
-    }
+    public IntentsTestRule<AdminProfileNavigation> intentsTestRule = new IntentsTestRule<AdminProfileNavigation>(AdminProfileNavigation.class);
 
 
     @Test
     public void recyclTest(){
        Espresso.onView(withId(R.id.recycler_menu)).perform(RecyclerViewActions.actionOnItemAtPosition(1,click()));
 
-        RecyclerView recyclerView = adminProfileNavigationActivityTestRule.getActivity().findViewById(R.id.recycler_menu);
-
-        int itemcount = recyclerView.getAdapter().getItemCount();
-
-        Espresso.onView(withId(R.id.recycler_menu)).perform(RecyclerViewActions.scrollToPosition(itemcount - 1));
-
-        onView(withId(R.id.recycler_menu)).perform(RecyclerViewActions.actionOnItemAtPosition(ITEMBELOWFINDINLIST,click()));
-
-        String itemval = "Fifth Title";
-        onView(withText(itemval)).check(matches(isDisplayed()));
-
-
-
     }
 
-
+    @Test
+    public void testOnNavigationItemSelected() {
+        onView(withId(R.id.nav_addpot)).perform(click());
+        intended(hasComponent(addnewpot.class.getName()));
+    }
 }
