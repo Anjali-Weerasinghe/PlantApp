358930d2def23fe0d504e8727b05281d9f2bd7d5
ANJALI WEERASINGHE <sapnaweerasinghe98@gmail.com>
2021-05-16 12:28:31 +0530

    Item Viewer

--- app/src/main/java/com/example/planthome/ItemViwer.java
+++ app/src/main/java/com/example/planthome/ItemViwer.java
@@ -2,6 +2,7 @@ package com.example.planthome;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.planthome.Model.Items;
 import com.example.planthome.ViewHolder.ItemViewHolder;
@@ -25,10 +27,9 @@ import com.google.firebase.database.annotations.NotNull;
 import com.squareup.picasso.Picasso;
 
 public class ItemViwer extends AppCompatActivity {
+       private Button addpot, addfertilizer, view;
 
-    private DatabaseReference ItemsRef;
-    private RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
+
 
 
 
@@ -36,61 +37,47 @@ public class ItemViwer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_viwer);
+        addpot = findViewById(R.id.addpot);
+        addfertilizer = findViewById(R.id.addfertilizer);
+        view = findViewById(R.id.view);
 
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ItemViwer.this,AdminProfileNavigation.class);
+                startActivity(intent);
 
+            }
+        });
+        addpot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ItemViwer.this,addnewpot.class);
+                startActivity(intent);
 
-        ItemsRef = FirebaseDatabase.getInstance().getReference().child("Pots");
-        recyclerView = findViewById(R.id.recycler_menu);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-    }
+            }
+        });
+        addfertilizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(ItemViwer.this,addnewfertilizer.class);
+                startActivity(intent);
+            }
+        });
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        FirebaseRecyclerOptions<Items> options =
-            new FirebaseRecyclerOptions.Builder<Items>()
-                    .setQuery(ItemsRef, Items.class)
-                    .build();
-
-        FirebaseRecyclerAdapter<Items, ItemViewHolder> adapter =
-                new FirebaseRecyclerAdapter<Items, ItemViewHolder>(options) {
-                    @Override
-                    protected void onBindViewHolder(@NonNull ItemViewHolder holder, int position, @NonNull Items model) {
-                        holder.txtItemCategory.setText(model.getCategory());
-                        holder.txtItemName.setText(model.getItemName());
-                        holder.txtItemPrice.setText("Price = Rs." + model.getPrice() + ".00");
-                        Picasso.get().load(model.getImage()).into(holder.imageView);
-
-                        holder.itemView.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Intent intent = new Intent(ItemViwer.this, ItemDetails.class);
-                                intent.putExtra("itemKey",model.getItemKey());
-                                startActivity(intent);
-
-
-                            }
-                        });
-
-                    }
-
-                    @NonNull
-                    @Override
-                    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pots_and_fertilizer_viewer, parent, false);
-                        ItemViewHolder holder = new ItemViewHolder(view);
-                        return holder;
-                    }
-
-
-                };
-        recyclerView.setAdapter(adapter);
-        adapter.startListening();
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
 }
